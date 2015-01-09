dcd2367e79359e346d894c4516cf4b95109924f6
jnavarro90 <jnavarroa90@gmail.com>
2015-01-09 22:35:04 +0100

    Practica 6
    Terminada

--- Java/Sockets/src/sockets/Sockets.java
+++ Java/Sockets/src/sockets/Sockets.java
@@ -1,5 +1,7 @@
 package sockets;
 
+import com.sun.security.ntlm.Client;
+
 /**
  *
  * @author javinavarroalaman
@@ -17,12 +19,19 @@ public class Sockets {
     } catch (InterruptedException e) {
         e.printStackTrace();
     }
+    Cliente c6 = new Cliente(900, "Luis");
     Cliente c3 = new Cliente(9000, "Pepe", "626371576");
     Cliente c4 = new Cliente(9000, "Javi", "626371213");
     Cliente c1 = new Cliente(9000, "Javi", "626371213");
     Cliente c2 = new Cliente(9000, "Javi");
     c1.start();
     c2.start();
+    try {
+        Thread.sleep(10 * 1000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+    c6.start();
     c4.start();
     try {
         Thread.sleep(10 * 1000);
